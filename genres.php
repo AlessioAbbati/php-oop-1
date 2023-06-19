8868c7136eabd2193c98847fc953b7050e42643c
AlessioAbbati <alex_117@live.it>
2023-06-19 18:39:48 +0200

    mod  genre construct

--- genres.php
+++ genres.php
@@ -1,32 +1,11 @@
 <?php
 
-class genres {
-    public string $genre1;
-    public string $genre2;
-    public string $genre3;
-    public string $genre4;
-    public string $genre5;
-    public string $genre6;
-    public string $genre7;
-    public string $genre8;
- 
-    // public function __construct(
-    //     string $genre1, 
-    //     string $genre2, 
-    //     string $genre3, 
-    //     string $genre4, 
-    //     string $genre5, 
-    //     string $genre6, 
-    //     string $genre7, 
-    //     string $genre8
-    //     ) {
-    //     $this->genre1 = $genre1;
-    //     $this->genre2 = $genre2;
-    //     $this->genre3 = $genre3;
-    //     $this->genre4 = $genre4;
-    //     $this->genre5 = $genre5;
-    //     $this->genre6 = $genre6;
-    //     $this->genre7 = $genre7;
-    //     $this->genre8 = $genre8;
-    // }
+class genres
+{
+    public string $genre;
+
+    public function __construct(string $genre,)
+    {
+        $this->genre = $genre;
+    }
 }
